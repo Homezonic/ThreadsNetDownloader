93a094bd67d883315c0f6aa14b697bc3c0e586cb
Akande Joshua <homezonic@gmail.com>
2023-07-07 02:01:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,18 +13,7 @@ app.use(bodyParser.json());
 
 // Define the endpoint to handle the search request
 app.post('/search', async (req, res) => {
-    const browser = await puppeteer.launch({
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote",
-        ],
-        executablePath:
-            process.env.NODE_ENV === "production"
-                ? process.env.PUPPETEER_EXECUTABLE_PATH
-                : puppeteer.executablePath(),
-    });
+    );
     try {
         const { url } = req.body;
 
